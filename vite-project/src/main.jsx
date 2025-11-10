73c46c0050b7a2563f43777efd0244c60dbdb29e
usernameXD00 <usernamexd00@gmail.com>
2025-11-10 12:43:57 +0530

    error in main.jsx resolved

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 // import App from './App.jsx'
 // import App from './App2.jsx'
-import App from './App3.jsx';
+import App3 from './App3.jsx';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App3 />);
