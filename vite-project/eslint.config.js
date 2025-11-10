127aba1b28a0898aa25ea715ed79a9be108bc709
usernameXD00 <usernamexd00@gmail.com>
2025-11-10 12:29:24 +0530

    added some configs in eslint.config

--- vite-project/eslint.config.js
+++ vite-project/eslint.config.js
@@ -1,9 +1,9 @@
 export default [
   {
-    files: ["**/*.js"], //only check .js files in server directory
+    files: ['**/*.{js,jsx}'], //only check .js files in server directory
     rules: {
-      semi: "error", //force semicolons
-      "no-unused-vars": "warn", //warn if variables are unused
+      semi: 'error', //force semicolons
+      'no-unused-vars': 'warn', //warn if variables are unused
     },
   },
 ];
